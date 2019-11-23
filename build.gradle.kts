86edadf924ee9f5e078e430fe73b16127bace91c
Harystolho <haristolhogamer@gmail.com>
2019-11-23 15:07:10 -0300

    Add gradle tasks to build fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,25 @@
 plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
     id("org.jetbrains.kotlin.jvm").version("1.3.31")
 
-    // Apply the application plugin to add support for building a CLI application.
     application
 }
 
 repositories {
-    // Use jcenter for resolving dependencies.
-    // You can declare any Maven/Ivy/file repository here.
     jcenter()
+    mavenCentral()
 }
 
 dependencies {
-    // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+    implementation("io.github.microutils:kotlin-logging:1.7.7")
+    implementation("org.slf4j:slf4j-simple:1.7.26")
 }
 
 application {
-    // Define the main class for the application
     mainClassName = "com.harystolho.hackathon.MainKt"
 }
 
@@ -33,5 +28,5 @@ tasks.withType<Jar> {
         attributes("Main-Class" to application.mainClassName)
     }
 
-    from(configurations.compileClasspath.get().map { if (it.isDirectory()) it else zipTree(it) })
+    from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
